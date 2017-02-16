02de1d0a6d1f7983101aaa455bf4f970e0e08b65
seanzihunterelli <sean.s.hoffman@gmail.com>
2017-02-15 21:48:26 -0700

    added GET and POST methods

--- js/messageService.js
+++ js/messageService.js
@@ -1,10 +1,16 @@
 angular.module('chatroom').service('messageService', function($http){
   //Here you'll need to create two methods. One called postMessage and the other called getMessages.
 
+
   //On the lines below create a getMessages method. This method will retrieve data from the backend.
   //The url for the get request should be 'https://practiceapi.devmounta.in/api/chats'
   //Be sure to return whatever gets returned from $http so you can call .then in your controller.
-
+  this.getMessages = function(){
+    return $http({
+      method:'GET',
+      url:'https://practiceapi.devmounta.in/api/chats'
+    });
+  };
 
 
 
@@ -13,7 +19,22 @@ angular.module('chatroom').service('messageService', function($http){
   //Because we're making a POST request, need a data object with a message property IE data: {message: "Hello World"}
   //Also, remember that $http returns a promise. So return the whole $http call (return $http(...)), so you can then use .then in your controller.
 
+  this.postMessage = function(newMessage){
+    return $http({
+      method:'POST',
+      url:'https://practiceapi.devmounta.in/api/chats',
+      data: {message: newMessage}
 
+    });
+  };
+
+  this.postCookie = function(cookie){
+
+  };
+
+  this.getCookies = function(){
+    $http.GET('https://practiceapi.devmounta.in/api/cookies');
+  };
 
 
 });
